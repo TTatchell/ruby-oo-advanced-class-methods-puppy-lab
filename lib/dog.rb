33918b89c306739a81b051d6140873a05d36b37c
TTatchell <tim.tatchell24@gmail.com>
2021-07-06 20:54:42 +1000

    upload

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,32 @@
-# Add your code here
+class Dog
+    @@all = []
+    attr_accessor :name
+
+    def initialize(name)
+        @name = name
+        save()
+    end
+
+    def self.all
+        @@all
+      end
+
+    def self.clear_all
+        @@all = []
+    end
+
+    def self.print_all
+        @@all.each do |dog|
+            puts dog.name
+        end
+    end
+
+    def save
+        @@all << self
+    end
+end
+
+Dog.new("Pluto")
+
+Dog.new("Fido")
+Dog.new("Maddy")
